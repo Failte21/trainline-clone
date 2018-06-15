f685f2dd39b4baa50f6eed759bca0631736c9f2f
failte <lsimon@student.42.fr>
2018-06-15 12:40:48 +0200

    wireframe: Create autoComplete component

--- src/app/side-autocomplete/side-autocomplete.component.ts
+++ src/app/side-autocomplete/side-autocomplete.component.ts
@@ -3,9 +3,13 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-autocomplete',
   templateUrl: './side-autocomplete.component.html',
-  styleUrls: ['./side-autocomplete.component.css']
+  styleUrls: ['./side-autocomplete.component.css', '../commons/commons.css']
 })
 export class SideAutocompleteComponent implements OnInit {
+
+  private cityList: String[] = [
+    'Paris', 'Paris', 'Paris', 'Paris', 'Paris', 'Paris'
+  ];
 
   constructor() { }
 
