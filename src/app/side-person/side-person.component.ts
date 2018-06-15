51e9f598191d1ed2ab7c2799aa3a3cb33a9025d9
failte <lsimon@student.42.fr>
2018-06-15 13:37:14 +0200

    wireframe: Create persons component

--- src/app/side-person/side-person.component.ts
+++ src/app/side-person/side-person.component.ts
@@ -3,10 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-side-person',
   templateUrl: './side-person.component.html',
-  styleUrls: ['./side-person.component.css']
+  styleUrls: ['./side-person.component.css', '../commons/commons.css']
 })
 export class SidePersonComponent implements OnInit {
 
+  private ageRanges: String[] = ['Adult (26-59)', 'Youth (0-25)', 'Senior (60+)'];
   constructor() { }
 
   ngOnInit() {
